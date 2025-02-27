4e492a16b16c7baa2fb5ffb60d2d713da4ea7314
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-27 17:25:42 +0100

    edit for loop + add eventlistener

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 /* Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
 const emailEl = document.querySelector(".randomEmail")
-
-let email = []
+const btnEl = document.getElementById("btn")
 
 function getEmails() {
-    for (let i = 0; i < emailEl.length; i++) {
-        const element = emailEl[i];
+    for (let i = 0; i < 10; i++) {
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.response);
+                const mail = data.response
+                emailEl.innerHTML += `<li>${mail}</li>`
+            })
+            .catch (error => {
+                //
+                console.log(error);
+            })
     }
 }
 
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch (error => {
-        //
-        console.log(error);
-    })
+getEmails()
+
+btnEl.addEventListener("click", function () {
+    emailEl.innerHTML = ""
+    getEmails()
+})
